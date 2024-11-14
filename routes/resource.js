7a6a9c76ea72c1b4e5d378867705bea6a1edd3b6
medabalimisowmya <medabalimisowmya@gmail.com>
2024-11-13 20:00:55 -0600

    modified resource.js file

--- routes/resource.js
+++ routes/resource.js
@@ -1,3 +1,5 @@
+// routes/resource.js
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,14 +7,14 @@ var router = express.Router();
 var api_controller = require('../controllers/api');
 var device_controller = require('../controllers/devices');
 
-// API ROUTE
+// API Route for listing resources
 router.get('/', api_controller.api);
 
 // DEVICE ROUTES
-router.post('/devices', device_controller.device_create_post);
-router.delete('/devices/:id', device_controller.device_delete);
-router.put('/devices/:id', device_controller.device_update_put);
-router.get('/devices/:id', device_controller.device_detail);
-router.get('/devices', device_controller.device_list);
+router.post('/devices', device_controller.device_create_post); // Create a device
+router.delete('/devices/:id', device_controller.device_delete); // Delete a device
+router.put('/devices/:id', device_controller.device_update_put); // Update a device
+router.get('/devices/:id', device_controller.device_detail); // Get details for a device
+router.get('/devices', device_controller.device_list); // List all devices
 
 module.exports = router;
