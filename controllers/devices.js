bb4acbc63ee8e78fd1012aa324caac90ca55d5a2
medabalimisowmya <medabalimisowmya@gmail.com>
2024-11-13 20:24:42 -0600

    printed documents in a collection

--- controllers/devices.js
+++ controllers/devices.js
@@ -3,27 +3,44 @@
 // Import the model
 var Device = require('../models/device');
 
-// List of all devices
-exports.device_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Device list');
+// List of all devices (Fetch all devices)
+exports.device_list = async function(req, res) {
+  try {
+    // Fetch all devices from MongoDB
+    const devices = await Device.find();  // This does not modify the database
+    res.json(devices);  // Send the list of devices as a JSON response
+  } catch (err) {
+    console.error('Error fetching devices:', err);
+    res.status(500).send(`{"error": ${err}}`);  // Send error if any
+  }
 };
 
-// Detail for a specific device
-exports.device_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Device detail: ' + req.params.id);
+// Detail for a specific device (Fetch details of a single device)
+exports.device_detail = async function(req, res) {
+  try {
+    const device = await Device.findById(req.params.id);  // Fetch device by ID
+    if (device) {
+      res.json(device);  // Send the details of the device
+    } else {
+      res.status(404).send('Device not found');
+    }
+  } catch (err) {
+    console.error('Error fetching device details:', err);
+    res.status(500).send(`{"error": ${err}}`);  // Send error if any
+  }
 };
 
-// Handle device create on POST
+// Handle device create on POST (Not implemented in your case yet)
 exports.device_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Device create POST');
+  res.send('Device create POST not implemented');
 };
 
-// Handle device delete on DELETE
+// Handle device delete on DELETE (Not implemented in your case yet)
 exports.device_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: Device delete DELETE ' + req.params.id);
+  res.send('Device delete DELETE not implemented');
 };
 
-// Handle device update on PUT
+// Handle device update on PUT (Not implemented in your case yet)
 exports.device_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Device update PUT ' + req.params.id);
+  res.send('Device update PUT not implemented');
 };
--- routes/devices.js
+++ routes/devices.js
@@ -1,15 +1,16 @@
+// routes/devices.js
+
 var express = require('express');
 var router = express.Router();
-var Device = require('../models/device'); // Make sure the path to the model is correct
+var Device = require('../models/device'); // Correct model import
 
-/* GET devices page. */
+/* GET devices page - List all devices */
 router.get('/', async function(req, res, next) {
   try {
     // Fetch all devices from MongoDB
     const devices = await Device.find();
-
     // Render the devices.pug page and pass the devices data
-    res.render('devices', { title: 'Search Results for Devices', results: devices });
+    res.render('devices', { title: 'Devices List', results: devices }); // Pass data to pug view
   } catch (err) {
     console.error('Error fetching devices:', err);
     res.status(500).send('Error fetching devices');
