9abf6957667232abe04fb53c8f3fca97fb5d16e7
medabalimisowmya <medabalimisowmya@gmail.com>
2024-12-04 16:21:01 -0600

    added input validation

--- models/device.js
+++ models/device.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 // Define the schema for the device
 const deviceSchema = new mongoose.Schema({
-  device_name: { type: String, required: true },
-  model: { type: String, required: true },
-  power_usage: { type: Number, required: true }
+  device_name: { type: String, required: true ,minlength:[20,"min value should be greater than 20 "]},
+  model: { type: String, required: true,minglength:[30,"min value should be greater than 30"] },
+  power_usage: { type: Number, required: true, min:[350,"power usage should be greater than 350"] }
 });
 
 // Create and export the Device model
